1add4181de856ff4e3c65ca2b517935f065ed54d
Yves Junqueira <yves.junqueira@gmail.com>
2016-03-13 00:43:41 -0800

    Make the file watcher work with the real game
    It must watch different temp files and not the original one as
    previously thought. The watches are working now.

--- client/watch_saved_games.go
+++ client/watch_saved_games.go
@@ -7,10 +7,12 @@ import (
 	"log"
 	"os"
 	"path"
+	"strings"
 
 	"path/filepath"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/streadway/amqp"
 )
 
 func stardewFolder() string {
@@ -21,13 +23,15 @@ func allSaveGameInfos() ([]string, error) {
 	return filepath.Glob(stardewFolder() + "/*/SaveGameInfo")
 }
 
-func main() {
-
-	ch, close, err := rabbitStart()
+func isDir(path string) bool {
+	fileInfo, err := os.Stat(path)
 	if err != nil {
-		log.Fatal(err)
+		return false
 	}
-	defer close()
+	return fileInfo.IsDir()
+}
+
+func watchAndPublish(topic *amqp.Channel) {
 
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
@@ -35,42 +39,70 @@ func main() {
 	}
 	defer watcher.Close()
 
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				if event.Op&fsnotify.Write == fsnotify.Write {
-					log.Println("modified file:", event.Name)
-					content, err := ioutil.ReadFile(event.Name)
-					if err != nil {
-						log.Printf("could not read file %v: %v", event.Name, err)
-						continue
-					}
-					if err := publishSavedGame(ch, content); err != nil {
-						log.Print("could not publish new save game content:", err)
-						continue
-					}
-					log.Print("New save game published")
-
-				}
-			case err := <-watcher.Errors:
-				log.Println("error:", err)
-			}
-		}
-	}()
-
 	gameSaves, err := allSaveGameInfos()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Find new folders containing new saved games.
+	err = watcher.Add(stardewFolder())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	watched := map[string]bool{}
+
 	for _, save := range gameSaves {
 		err = watcher.Add(save)
 		if err != nil {
 			log.Fatal(err)
 		}
 		log.Printf("Watching %v", save)
+		watched[save] = true
 	}
 
-	select {}
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("Found modified file:", event.Name)
+					switch {
+					case isDir(event.Name):
+						if !watched[event.Name] {
+							watcher.Add(event.Name)
+							watched[event.Name] = true
+						}
+						// The game is saved in a temporary file first (Directory/SaveGameInfo_STARDEWVALLEYTMP)
+					case strings.Contains(path.Base(event.Name), "SaveGameInfo"):
+						content, err := ioutil.ReadFile(event.Name)
+						if err != nil {
+							log.Printf("Could not read file %v: %v", event.Name, err)
+							continue
+						}
+						if err := publishSavedGame(topic, content); err != nil {
+							log.Print("could not publish new save game content:", err)
+							continue
+						}
+						log.Print("New save game published")
+					}
+				}
+			case err := <-watcher.Errors:
+				log.Println("error:", err)
+			}
+		}
+	}()
+	<-done
+}
 
+func main() {
+
+	topic, close, err := rabbitStart()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
+	watchAndPublish(topic)
 }
