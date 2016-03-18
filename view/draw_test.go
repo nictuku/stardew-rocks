4777fee2f2500153fb250e7e32294c0ca21d3713
Yves Junqueira <yves.junqueira@gmail.com>
2016-03-18 03:46:22 -0400

    Use Kylindra's save for testing

--- view/draw_test.go
+++ view/draw_test.go
@@ -19,7 +19,7 @@ func TestTileCoordinate(t *testing.T) {
 func TestLoadTile(t *testing.T) {
 	farm := parser.LoadFarmMap()
 
-	sg, err := os.Open("../assets/saves/Jake_117702234")
+	sg, err := os.Open("../assets/saves/Aerlia-1458278945")
 	if err != nil {
 		t.Fatal(err)
 	}
