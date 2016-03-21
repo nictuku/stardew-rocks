227f6eeb421d077ed3670952f1a16177b231f57f
Yves Junqueira <yves.junqueira@gmail.com>
2016-03-20 22:33:38 -0700

    Parametrize the view/bin/draw.go output

--- view/bin/draw.go
+++ view/bin/draw.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Fatalf("Expected `%v <xml save file>`", os.Args[0])
+	if len(os.Args) != 3 {
+		log.Fatalf("Expected `%v <xml save file> <output.png>`", os.Args[0])
 	}
 	farm := parser.LoadFarmMap()
 
@@ -20,9 +20,14 @@ func main() {
 		log.Fatal(err)
 	}
 
+	f, err := os.OpenFile(os.Args[2], os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	gameSave, err := parser.ParseSaveGame(sg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	view.WriteImage(farm, gameSave, os.Stdout)
+	view.WriteImage(farm, gameSave, f)
 }
