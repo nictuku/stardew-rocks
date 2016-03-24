e1cf5350d0e1262582bb4c5d3e1ecfba17dce52b
Mitchel Spears <gumbyscout@gmail.com>
2016-03-23 23:42:03 -0500

    Dont' use map, because it won't let you

--- web/client/app/farm/farm.service.ts
+++ web/client/app/farm/farm.service.ts
@@ -1,7 +1,6 @@
 /// <reference path="../../typings/main.d.ts"/>
 import {Injectable} from 'angular2/core';
 import {Http, Response} from 'angular2/http';
-import {Observable}     from 'rxjs/Observable';
 
 export interface Farm {
   id: string,
@@ -18,21 +17,22 @@ export class FarmService {
   constructor(private _http: Http){};
 
   getFarms () {
-    return this._http.get("api/farms")
-      .map(res => <Farm[]>res.json().data)
-      .catch(this.handleError)
-      .toPromise();
+    return new Promise((resolve, reject) => {
+      this._http.get("api/farms")
+        .subscribe(res => {
+          console.log(res.json());
+          resolve(res.json());
+        });
+    });
   }
 
   getFarm (id: string) {
-    return this._http.get(`api/farm/${id}`)
-      .map(res => <Farm>res.json().data)
-      .catch(this.handleError)
-      .toPromise();
-  }
-
-  private handleError (error: Response) {
-    console.error(error);
-    return Observable.throw(error.json().error || 'Server error');
+    return new Promise((resolve, reject) => {
+      this._http.get(`api/farm/${id}`)
+        .subscribe(res => {
+          console.log(res.json());
+          resolve(res.json());
+        });
+    });
   }
 };
