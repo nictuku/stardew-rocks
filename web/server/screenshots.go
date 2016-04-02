c7af07c13c4f64070749a5035ac1e0f6e30719ad
Yves Junqueira <yves.junqueira@gmail.com>
2016-04-01 22:13:46 -0700

    Also requesting screenshot resize without a query parameter
    This will make the images more likely to be cached, which is importat
    since we're adding scrolling on the landing page.
    
    Example valid screenshot:
    
    http://localhost:8080/screenshot/56f34dd6f48e3e520cea687b/1458785750w350.png

--- web/server/screenshots.go
+++ web/server/screenshots.go
@@ -36,6 +36,20 @@ func LegacyMap(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+func widthPath(filepath string) (normalizedPath, width string, ok bool) {
+	spl := strings.SplitN(filepath, ".", 2)
+
+	if len(spl) != 2 {
+		return filepath, "", false
+	}
+	suf := strings.SplitN(spl[0], "w", 2)
+	if len(suf) != 2 {
+		return filepath, "", false
+	}
+	return suf[0] + "." + spl[1], suf[1], true
+
+}
+
 func ServeScreenshot(w http.ResponseWriter, r *http.Request) {
 	if !strings.HasPrefix(r.URL.Path, "/screenshot") {
 		http.Error(w, "Not found"+r.URL.Path, http.StatusNotFound)
@@ -54,16 +68,31 @@ func ServeScreenshot(w http.ResponseWriter, r *http.Request) {
 		f   io.Reader
 		err error
 	)
-	if f, err = stardb.GFS.Open(r.URL.Path); err != nil {
+
+	path := r.URL.Path
+
+	// The target width can be specified by a ?w=<value> parameter, or by
+	// a w<number>.png suffix in the file basename.
+
+	width := r.URL.Query().Get("w")
+	if p, wp, ok := widthPath(s[3]); ok {
+		width = wp
+		path = strings.Join(append(s[0:3], p), "/")
+	}
+
+	if f, err = stardb.GFS.Open(path); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	width := r.URL.Query().Get("w")
 	if width != "" {
 		wid, err := strconv.Atoi(width)
-		if err != nil || wid < 100 || wid > 500 {
+		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		if wid < 100 || wid > 500 {
+			http.Error(w, "Invalid range", http.StatusBadRequest)
 			return
 		}
 
