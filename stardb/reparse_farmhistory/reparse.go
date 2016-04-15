759a62ddc45fd4fc53c469f2250bf5379eac0bae
Yves Junqueira <yves.junqueira@gmail.com>
2016-04-15 00:08:41 -0400

    parallel execution

--- stardb/reparse_farmhistory/reparse.go
+++ stardb/reparse_farmhistory/reparse.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/nictuku/stardew-rocks/parser"
 	"github.com/nictuku/stardew-rocks/stardb"
@@ -26,38 +27,55 @@ func main() {
 	c := stardb.AllFarms(farmerRE)
 	totalFarms := 0
 	totalSaves := 0
-	for farm := range c {
-		totalFarms++
-		fmt.Println("processing", farm.Farmer)
+	mu := new(sync.Mutex)
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			for farm := range c {
+				fmt.Println("processing", farm.Farmer)
 
-		savetimes := []int{}
-		if *lastOnly {
-			savetimes = []int{int(farm.LastUpdate.Unix())}
-		} else {
-			savetimes = farm.SaveTimes()
-		}
-		for _, st := range savetimes {
-			gfile := stardb.SaveGamePathInt(farm.InternalID.Hex(), st)
-			log.Println(gfile)
+				savetimes := []int{}
+				if *lastOnly {
+					savetimes = []int{int(farm.LastUpdate.Unix())}
+				} else {
+					savetimes = farm.SaveTimes()
+				}
+				for _, st := range savetimes {
+					gfile := stardb.SaveGamePathInt(farm.InternalID.Hex(), st)
+					log.Println(gfile)
 
-			sg, err := stardb.GFS.Open(gfile)
-			if err != nil {
-				log.Fatal(err)
+					sg, err := stardb.GFS.Open(gfile)
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+					gameSave, err := parser.ParseSaveGame(sg)
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+					fi, err := stardb.FarmHistoryFromSaveGame(farm.InternalID, gameSave, st)
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+					if err := stardb.InsertFarmHistory(farm.InternalID, fi); err != nil {
+						log.Println(err)
+						continue
+					}
+					mu.Lock()
+					totalSaves++
+					mu.Unlock()
+				}
+				mu.Lock()
+				totalFarms++
+				mu.Unlock()
+				log.Printf("Done reparsing %v with %v items\n", farm.Farmer, len(savetimes))
 			}
-			gameSave, err := parser.ParseSaveGame(sg)
-			if err != nil {
-				log.Fatal(err)
-			}
-			fi, err := stardb.FarmHistoryFromSaveGame(farm.InternalID, gameSave, st)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if err := stardb.InsertFarmHistory(farm.InternalID, fi); err != nil {
-				log.Fatal(err)
-			}
-			totalSaves++
-		}
-		log.Printf("Done reparsing %v with %v items\n", farm.Farmer, len(savetimes))
+			wg.Done()
+		}()
 	}
+	wg.Wait()
 	log.Printf("Updated %d farms and %d save games", totalFarms, totalSaves)
 }
