99117b96c7db7e5d5f607a739c8989bfdbbbc3b5
Yves Junqueira <yves.junqueira@gmail.com>
2016-04-23 05:49:02 +0200

    Various deploy.sh changes
    - use a single root
    - restart the server after the update
    - copy the i18n directory

--- web/client/deploy.sh
+++ web/client/deploy.sh
@@ -20,7 +20,7 @@ mkdir -p "${dest}"
 tmpdest="$(mktemp -d)"
 
 # "content" is currently using an absolute directory, so don't copy it.
-cp -R index.html bundle.js bundle.js.map favicon.ico favicon-152.png "${tmpdest}"
+cp -R index.html bundle.js bundle.js.map favicon.ico favicon-152.png i18n "${tmpdest}"
 
 # "content" is currently using an absolute directory
 # This changes the live stuff.
@@ -37,3 +37,11 @@ sed -e "s#assets/[0-9]\+/#assets/${ts}/#" \
 	/etc/apache2/sites-available/${release_env}.conf > "${tmp}"
 
 cp "${tmp}" /etc/apache2/sites-available/${release_env}.conf
+ln -s /home/stardew/www-${release_env}/assets/${ts} /home/stardew/www-{release_env}/current
+
+# do this once:
+# /home/stardew/www-prod/current /home/stardew/www
+
+# Needs this in /etc/sudoers:
+# stardew   ALL=(ALL) NOPASSWD: /usr/sbin/apache2ctl
+sudo apache2ctl graceful
