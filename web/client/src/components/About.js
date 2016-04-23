0013e866f607a6d7c72e42eb01d3c0aec50bde2a
Mitchel Spears <gumbyscout@gmail.com>
2016-04-22 23:49:26 -0500

    Made about page tranlateable

--- web/client/src/components/About.js
+++ web/client/src/components/About.js
@@ -1,10 +1,26 @@
 import React, {PropTypes} from 'react';
+import Radium from 'radium';
 import Paper from 'material-ui/lib/paper';
+import {FormattedMessage, FormattedHTMLMessage, injectIntl, intlShape, defineMessages} from 'react-intl';
 import color from 'color';
 
 import colors from '../colors';
 
+const messages = defineMessages({
+  screenshotAlt: {
+    id: 'about.screenshotAlt',
+    description: 'alt text for taskbar screenshot',
+    defaultMessage: 'taskbar screenshot'
+  }
+});
+
+@injectIntl
+@Radium
 class About extends React.Component {
+  static propTypes = {
+    intl: intlShape.isRequired
+  };
+
   static contextTypes = {
     season: PropTypes.string
   };
@@ -33,46 +49,135 @@ class About extends React.Component {
     return (
       <div style={this.styles().about}>
         <Paper style={this.styles().paper}>
-          <h2>About</h2>
+          <h2>
+            <FormattedMessage
+              id="about.aboutHeader"
+              defaultMessage="About"
+              description="about header"
+            />
+          </h2>
           <p>
-            Stardew Farm is a tool for Stardew Valley that collects saved games and shares them with our community. We create and share screenshots of your game with everyone - no complicated setup needed!
+            <FormattedMessage
+              id="about.paragraph1"
+              description="summary of application"
+              defaultMessage="Stardew Farm is a tool for Stardew Valley that collects saved games and shares them with our community. We create and share screenshots of your game with everyone - no complicated setup needed!"
+            />
           </p>
           <p>
-            Install the Stardew Farm client to share your farm state! Farm screenshots will automatically appear here every time your game is saved.
+            <FormattedMessage
+              id="about.paragraph2"
+              description="summary of how to upload farms"
+              defaultMessage="Install the Stardew Farm client to share your farm state! Farm screenshots will automatically appear here every time your game is saved."
+            />
           </p>
           <a href="https://github.com/nictuku/stardew-rocks/releases/latest">
-            Download Stardew Farm!
+            <FormattedMessage
+              id="about.callToAction"
+              description="the link to download the desktop client"
+              defaultMessage="Download Stardew Farm!"
+            />
           </a>
           <p>
-            Just download the .exe file and run it. It stays on your system tray and looks like this:
+            <FormattedMessage
+              id="about.instructions1"
+              description="how to use desktop client primer"
+              defaultMessage="Just download the .exe file and run it. It stays on your system tray and looks like this:"
+            />
           </p>
-          <img alt="Screenshot" src="content/systray.png" />
+          <img alt={this.props.intl.formatMessage(messages.screenshotAlt)} src="content/systray.png" />
           <p>
-            You can right click on that icon to close it.
+            <FormattedMessage
+              id="about.instructions2"
+              description="how to close desktop client"
+              defaultMessage="You can right click on that icon to close it."
+            />
           </p>
           <p>
-            The app is configured to run on startup for you. If you don't want that, just rename the stardew_rocks.exe executable to something else and it won't start automatically anymore.
+            <FormattedMessage
+              id="about.instructions3"
+              description="how to make the app not run at startup"
+              defaultMessage="The app is configured to run on startup for you. If you don't want that, just rename the stardew_rocks.exe executable to something else and it won't start automatically anymore."
+            />
           </p>
-          <h2>FAQ</h2>
-          <h4>How do I find my farm?</h4>
+          <h2>
+            <FormattedMessage
+              id="faq.faqHeader"
+              description="header for faq"
+              defaultMessage="FAQ"
+            />
+          </h2>
+          <h4>
+            <FormattedMessage
+              id="faq.findFarmHeader"
+              description="header for how to find one's farm"
+              defaultMessage="How do I find my farm?"
+            />
+          </h4>
           <p>
-            After you have slept one night in game with this service running, use the search bar at the top and enter your farm or farmer's name.
+            <FormattedMessage
+              id="faq.findFarm"
+              description="how to find one's farm"
+              defaultMessage="After you have slept one night in game with this service running, use the search bar at the top and enter your farm or farmer's name."
+            />
           </p>
-          <h4>I don't see my screenshot. What happened?</h4>
+          <h4>
+            <FormattedMessage
+              id="faq.findScreenshotHeader"
+              description="header for where's my screenshot"
+              defaultMessage="I don't see my screenshot. What happened?"
+            />
+          </h4>
           <p>
-            The save files are only uploaded after the game is saved, which happens after each night in the game.
+            <FormattedMessage
+              id="faq.findScreenshot"
+              description="where's my screenshot"
+              defaultMessage="The save files are only uploaded after the game is saved, which happens after each night in the game."
+            />
           </p>
-          <h4>There's something missing on my farm :-(</h4>
+          <h4>
+            <FormattedMessage
+              id="faq.missingItemsHeader"
+              description="header for missing items on farm"
+              defaultMessage="There's something missing on my farm :-("
+            />
+          </h4>
           <p>
-            We are still working on the tool and not everything is perfect yet. Check again in a few days :). Once we update the code, we go back and regenerate all screenshots, so nothing is lost.
+            <FormattedMessage
+              id="faq.missingItems"
+              description="stuff is missing on my farm"
+              defaultMessage="  We are still working on the tool and not everything is perfect yet. Check again in a few days :). Once we update the code, we go back and regenerate all screenshots, so nothing is lost."
+            />
           </p>
-          <h4>So what do you save?</h4>
+          <h4>
+            <FormattedMessage
+              id="faq.whatIsSavedHeader"
+              description="header for what is saved"
+              defaultMessage="So what do you save?"
+            />
+          </h4>
           <p>
-            We save the save file for each and every day of your game, as long as the client is up and running. You can click on your farm on the site and look at it.
+            <FormattedMessage
+              id="faq.whatIsSaved"
+              description="what is uploaded to us"
+              defaultMessage="We save the save file for each and every day of your game, as long as the client is up and running. You can click on your farm on the site and look at it."
+            />
           </p>
-          <h4>Something else is not working, I need help!</h4>
+          <h4>
+            <FormattedMessage
+              id="faq.needHelpHeader"
+              description="header for I need help"
+              defaultMessage="Something else is not working, I need help!"
+            />
+          </h4>
           <p>
-            Just join our <a href="https://discord.gg/0tpEyZrnOVQKA93b">Discord server chat</a> and ask!
+            <FormattedHTMLMessage
+              id="faq.needHelp"
+              description="link to support discord channel"
+              defaultMessage="Just join our <a href='{discordLink}'>Discord server chat</a> and ask!"
+              values={{
+                discordLink: "https://discord.gg/0tpEyZrnOVQKA93b"
+              }}
+            />
           </p>
         </Paper>
       </div>
