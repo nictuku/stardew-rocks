5f9fa9b4d62c123a7ce83653f9c567d2d575be41
Mitchel Spears <gumbyscout@gmail.com>
2016-04-26 16:44:13 -0500

    Added styling to graph

--- web/client/src/components/Farm/StatsEarningsChart.js
+++ web/client/src/components/Farm/StatsEarningsChart.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-magic-numbers */
 import React, {PropTypes} from 'react';
+import {Style} from 'radium';
 import _ from 'lodash';
 import moment from 'moment';
 import d3 from 'd3';
@@ -35,8 +36,10 @@ class StatsEarningsChart extends React.Component {
         margin = {top: 20, right: 20, bottom: 120, left: 70},
         width = 800 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom,
-        x = d3.scale.linear().range([0, width]),
-        y = d3.scale.linear().range([height, 0]);
+        x = d3.scale.linear()
+          .range([0, width]).domain(d3.extent(earnings, d => d.date)),
+        y = d3.scale.linear()
+          .range([height, 0]).domain([0, d3.max(earnings, d => d.totalEarned)]);
 
     // make the container
     let svg = d3.select(node)
@@ -45,11 +48,9 @@ class StatsEarningsChart extends React.Component {
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    x.domain(d3.extent(earnings, d => d.date));
-    y.domain(d3.extent(earnings, d => d.totalEarned));
-
     // draw the dates axis
     svg.append('g')
+      .attr("class", "x axis")
       .attr('transform', `translate(0, ${height})`)
       .call(d3.svg.axis()
         .scale(x)
@@ -59,26 +60,54 @@ class StatsEarningsChart extends React.Component {
       .selectAll('text')
       .attr('x', 10)
       .attr('transform', 'rotate(90)')
+      .attr('dy', '-.25em')
       .style("text-anchor", "start");
 
     // draw the earnings axis
     svg.append('g')
+      .attr("class", "y axis")
       .call(d3.svg.axis().scale(y).orient('left'));
 
     // draw the lines
     svg.append('path')
       .datum(earnings)
-      .attr('style', {
-        fill: 'none',
-        stroke: 'steelblue',
-        strokeWidth: '.25rem'
-      })
+      .attr("class", "line")
       .attr('d', d3.svg.line()
         .x(d => x(d.date))
         .y(d => y(d.totalEarned))
       );
 
-    return node.toReact();
+    return (
+      <div className="earnings-chart">
+        <Style
+          scopeSelector=".earnings-chart"
+          rules= {{
+            '.axis path, .axis line': {
+              fill: 'none',
+              stroke: '#000',
+              shapeRendering: 'crispEdges'
+            },
+            '.x.axis path': {
+              display: 'none'
+            },
+            '.line': {
+              fill: 'none',
+              stroke: 'steelblue',
+              strokeWidth: '1.5px'
+            },
+            '.overlay': {
+              fill: 'none',
+              pointerEvents: 'all'
+            },
+            '.focus circle': {
+              fill: 'none',
+              stroke: 'steelblue'
+            }
+          }}
+        />
+        {node.toReact()}
+      </div>
+    );
   }
 }
 
